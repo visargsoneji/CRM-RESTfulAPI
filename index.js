60f8600620b486e39921888c077f775688f51904
visargsoneji <visargbs22@gmail.com>
2020-09-14 21:26:12 +0530

    main file

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import routes from './src/routes/crmRoutes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 5000;
+
+//mangoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb', {
+    useNewUrlParser : true, 
+    useUnifiedTopology : true
+});
+
+//body-parser setup
+app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.json());
+
+
+routes(app);
+
+app.get('/', (req,res) => {
+    res.send(`Node and express server running on PORT ${PORT}`);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
